c563ab9a405c571c4ad955d6c6c162da4e3d7946
alexanrodrigues <a74549@ualg.pt>
2023-11-28 20:27:34 +0000

    Update create-repos.py

--- .github/scripts/create-repos.py
+++ .github/scripts/create-repos.py
@@ -5,4 +5,6 @@ with open('.github/example-files/repo-example.json') as json_file:
     for object in json_file:
         repository = json.loads(object)
         repositories.append(repository)
-        print(repository)
+
+for repository in repositories:
+    print(repository["repo_name"])
