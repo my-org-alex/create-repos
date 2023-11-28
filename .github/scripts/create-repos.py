c0f12f08dba433b862c4ebc1607a4e5eedb1814c
alexanrodrigues <a74549@ualg.pt>
2023-11-28 20:36:46 +0000

    Update create-repos.py

--- .github/scripts/create-repos.py
+++ .github/scripts/create-repos.py
@@ -1,10 +1,20 @@
 import json
 
-repositories = []
-with open('.github/example-files/repo-example.json') as json_file:
-    for object in json_file:
-        repository = json.loads(object)
-        repositories.append(repository)
+data = json.loads('.github/example-files/repo-example.json')
 
-for repository in repositories:
-    print(repository["repo_name"])
+for repo in data["repositories"]:
+    print(f"Repository: {repo['repo_name']}")
+    
+    print("Environments:")
+    for env in repo["environments"]:
+        print(f"  {env['env_name']}, Production: {env['production']}")
+
+    print("Repository Variables:")
+    for var in repo["repo_variables"]:
+        print(f"  {var['repo_var_name']}: {var['value']}")
+
+    print("Repository Secrets:")
+    for secret in repo["repo_secrets"]:
+        print(f"  {secret['repo_secret_name']}: {secret['value']}")
+
+    print("\n")
