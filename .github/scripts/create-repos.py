0705c1f005bdcea979e315aa26ed5206fdf788c9
alexanrodrigues <a74549@ualg.pt>
2023-11-28 18:38:14 +0000

    Update create-repos.py

--- .github/scripts/create-repos.py
+++ .github/scripts/create-repos.py
@@ -1,6 +1,6 @@
 import json
 
-with open("../example-files/repo-example.json", "r") as read_json:
+with open(".github/example-files/repo-example.json", "r") as read_json:
     data = json.load(read_json)
 
 print(read_json)
