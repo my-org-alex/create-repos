b7e500ac30a7c29f5c69c1fdde1b3a54854cafab
alexanrodrigues <a74549@ualg.pt>
2023-11-29 08:59:22 +0000

    Update create-repos.py

--- .github/scripts/create-repos.py
+++ .github/scripts/create-repos.py
@@ -33,17 +33,77 @@ def make_api_call(endpoint):
         else:
             print(f"Error creating repository '{repo_name}'. Status code: {response.status_code}")
             print(response.text)
+        
+        print("Environments:")
+        for env in repo["environments"]:
+            print(f"  {env['env_name']}, Production: {env['production']}")
+            env_name = (f"{env['env_name']}")
+            env_prod = (f"{env['production']}")
+            
+            env_data = {
+            "name": env_name,    
+            "production": env_prod
+            }
+
+            headers = {
+                "Authorization": f"token {ACCESS_TOKEN}",
+                "Accept": "application/vnd.github.v3+json"
+            }
+            endpoint = "{API_ENDPOINT}/{repo_name}/environments/{env_name}"
+            response = requests.post(endpoint, json=env_data, headers=headers)
+
+            if response.status_code == 201:
+                print(f"Environment '{env_name}' created successfully.")
+            else:
+                print(f"Error creating environment '{env_name}'. Status code: {response.status_code}")
+                print(response.text)
+
+        print("Repository Variables:")
+        for var in repo["repo_variables"]:
+            print(f"  {var['repo_var_name']}: {var['value']}")
+            var_name = (f"{var['repo_var_name']}")
+            var_value = (f"{var['value']}")
+            
+            var_data = {
+            "name": var_name,
+            "value": var_value
+            }
+
+            headers = {
+                "Authorization": f"token {ACCESS_TOKEN}",
+                "Accept": "application/vnd.github.v3+json"
+            }
+            endpoint = "{API_ENDPOINT}/{repo_name}/actions/variables"
+            response = requests.post(endpoint, json=var_data, headers=headers)
+
+            if response.status_code == 201:
+                print(f"Variable '{var_name}' created successfully.")
+            else:
+                print(f"Error creating variable '{var_name}'. Status code: {response.status_code}")
+                print(response.text)
+
+        print("Repository Secrets:")
+        for secret in repo["repo_secrets"]:
+            print(f"  {secret['repo_secret_name']}: {secret['value']}")
+            secret_name = (f"{var['repo_secret_name']}")
+            secret_value = (f"{var['value']}")
+            
+            secret_data = {
+            "name": secret_name,
+            "value": secret_value
+            }
+
+            headers = {
+                "Authorization": f"token {ACCESS_TOKEN}",
+                "Accept": "application/vnd.github.v3+json"
+            }
+            endpoint = "{API_ENDPOINT}/{repo_name}/actions/secrets"
+            response = requests.post(endpoint, json=secret_data, headers=headers)
+
+            if response.status_code == 201:
+                print(f"Secret '{secret_name}' created successfully.")
+            else:
+                print(f"Error creating secret '{secret_name}'. Status code: {response.status_code}")
+                print(response.text)
 
 make_api_call("https://api.github.com/orgs/my-org-alex/repos")
-    
-    # print("Environments:")
-    # for env in repo["environments"]:
-    #     print(f"  {env['env_name']}, Production: {env['production']}")
-
-    # print("Repository Variables:")
-    # for var in repo["repo_variables"]:
-    #     print(f"  {var['repo_var_name']}: {var['value']}")
-
-    # print("Repository Secrets:")
-    # for secret in repo["repo_secrets"]:
-    #     print(f"  {secret['repo_secret_name']}: {secret['value']}")
