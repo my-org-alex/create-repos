f2bdb630b781f12c26c71089bd6419fb9df820f6
alexanrodrigues <a74549@ualg.pt>
2023-11-29 10:00:59 +0000

    Update create-repos.py

--- .github/scripts/create-repos.py
+++ .github/scripts/create-repos.py
@@ -105,4 +105,4 @@ def make_api_call(endpoint):
                 print(f"Error creating secret '{secret_name}'. Status code: {response.status_code}")
                 print(response.text)
 
-make_api_call("https://api.github.com/orgs/my-org-alex/repos")
+make_api_call("https://api.github.com/repos/my-org-alex")
